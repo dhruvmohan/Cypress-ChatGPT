5aa2766eee9282599a29d987e57b322e195879a3
dhruvmohan <mailtodhruvmohan@gmail.com>
2024-03-03 13:06:56 +0100

    3 Mar 2024

---
+++ cypress.config.js
@@ -0,0 +1,139 @@
+const { defineConfig } = require("cypress");
+
+
+module.exports = defineConfig({
+  e2e: {
+    chromeWebSecurity: true,
+    screenshotOnRunFailure: true,
+    watchForFileChanges: false,
+    screenshotsFolder: "cypress/results/mochawesome-report/assets",
+    env: {
+      log: {
+        level: "warn" // Set the log level globally to "warn"
+      }
+    },
+    // retries: {
+    //  runMode: 1,
+    //  openMode: 1,
+    //},
+    defaultCommandTimeout: 50000,
+    pageLoadTimeout: 120000,
+    reporter: "mochawesome",
+    reporterOptions: {
+      charts: true,
+      reportDir: "cypress/results",
+      reportFilename: "report",
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
+    video: true,
+ //   specPattern: ["**/*.feature", "cypress/e2e/*.cy.{js,jsx,ts,tsx}"],
+
+    async setupNodeEvents(on, config) {
+
+
+      on("task", {
+
+        seleniumBrowser: async ({ var_url, username, password }) => {
+          const chromeCapabilities = new chrome.Options();
+          const driver = await new Builder()
+            .forBrowser("chrome")
+            .setEdgeOptions(chromeCapabilities)
+            .build();
+          await driver.manage().window().maximize();
+          await driver.get(var_url)
+          await driver.findElement(By.id("wm_login-username")).sendKeys(username)
+          await driver.findElement(By.id("pw-input-view")).click()
+          await driver.findElement(By.id("wm_login-password")).click()
+          await driver.findElement(By.id("wm_login-password")).sendKeys(password)
+          await driver.findElement(By.id("submit_login")).click()
+          await driver.findElement(By.xpath('//a[text()="Kundenmanagement"]')).click();
+          await driver.findElement(By.xpath('//a[text()="Eingegangene Anmeldungen"]')).click();
+          await driver.findElement(By.xpath('//input[contains(@class, "input20")]')).click()
+          await driver.findElement(By.xpath('//input[contains(@class, "input20")]')).sendKeys("Stuttgart-Best")
+          await driver.sleep(5000);
+          await driver.findElement(By.xpath(`//*[contains(text(), 'Stuttgart-Best')]`)).click()
+          await driver.findElement(By.css('input[type="checkbox"]')).click()
+          await driver.findElement(By.xpath('//span[text()="Suchen"]')).click()
+          await driver.sleep(15000);
+          driver.close()
+          return driver;
+        },
+
+        Sperrung_Afheben: async ({ var_url, username, password }) => {
+          const chromeCapabilities = new chrome.Options();
+          const driver = await new Builder()
+            .forBrowser("chrome")
+            .setEdgeOptions(chromeCapabilities)
+            .build();
+          await driver.manage().window().maximize();
+          await driver.get(var_url)
+          await driver.findElement(By.id("wm_login-username")).sendKeys(username)
+          await driver.findElement(By.id("pw-input-view")).click()
+          await driver.findElement(By.id("wm_login-password")).click()
+          await driver.findElement(By.id("wm_login-password")).sendKeys(password)
+          await driver.findElement(By.id("submit_login")).click()
+          await driver.findElement(By.xpath('//a[text()="Administration"]')).click();
+          await driver.findElement(By.xpath('//a[text()="Belegungsplan"]')).click();
+          const elements = await driver.findElements(By.xpath('//input[contains(@class, "input20")]'));
+          await elements[1].click()        
+          await driver.sleep(5000); 
+          await elements[1].sendKeys("Stuttgart-Best")
+          await driver.sleep(5000);
+          await driver.findElement(By.xpath(`//*[contains(text(), 'Stuttgart-Best')]`)).click()
+          await driver.sleep(5000);
+          await driver.findElement(By.xpath('//span[text()="Suche"]')).click();
+          await driver.sleep(5000);
+          let elements1 = await driver.findElements(By.xpath('//input[contains(@class, "cbToDisable")]'));;
+          let secondElement1 = elements1[1];
+          await secondElement1.click();
+          await driver.sleep(5000);
+          {
+            let elements2 = await driver.findElements(By.xpath('//select[contains(@class, "panelToDisable")]'));
+            let secondElement2 = elements2[1];
+            await secondElement2.click();
+            await driver.sleep(5000)
+            let select = new Select(secondElement2);
+            await select.selectByIndex(0);
+            await driver.sleep(5000);
+          }
+          await driver.findElement(By.xpath('//span[text()="Speichern"]')).click();
+          await driver.sleep(5000);
+          await driver.findElement(By.xpath('//span[text()="Fortfahren"]')).click();
+          await driver.sleep(5000);
+          await driver.findElement(By.xpath('//a[text()="Logout "]')).click();
+          driver.close();
+          return driver;
+        }
+
+      });
+
+
+     
+
+    },
+
+
+
+  },
+
+
+});
+
+
+/*
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+*/
+
+
---
+++ cypress/e2e/chatgpt.spec.cy.js
@@ -0,0 +1,23 @@
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+  apiKey: 'your-chatGPT-api-key', dangerouslyAllowBrowser: true
+});
+
+describe('ChatGPT Integration', () => {
+  it('should generate a response', () => {
+  
+
+    async function runCompletion () {
+      const completion = await openai.chat.completions.create({
+      model: "gpt-4-0125-preview",
+      messages: [{ role: "user", content: "generate a 10000000000000 digit random number" }],
+      temperature: 0,
+      max_tokens: 1000,
+      });
+      cy.log(JSON.stringify(completion.choices[0].message.content));
+  }
+  runCompletion();
+
+  });
+});
